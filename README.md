fa3e32d778d36b837c7d3e011ee82340b8625813
Shinya Takahashi <s.takahashi313@gmail.com>
2024-08-04 21:41:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# sscg
+
+The **S**uper **S**imple **C**ode **G**enerator.
+
+> *This package prioritizes stability with minimal version updates. Dependencies are regularly updated and bugs are fixed, but new features are not added and existing ones are not changed. This approach results in fewer version bumps, keeping the package simple and its behavior consistent.*
+
+## Installation
+
+```
+$ npm install --save-dev sscg
+```
+
+## Getting started
+
+### 1. Create base directory
+
+Default base directory is `./templates`.
+
+```
+$ mkdir templates
+```
+
+### 2. Create your first template
+
+Now, let's create a template.
+
+```
+$ mkdir templates/sample
+```
+
+**The name of directory under the `templates` directory (here is `sample`) is treated as template name.**
+
+You can create multiple `tpl` files in the `template` directory.
+
+```
+$ touch templates/sample/{{kebabCase[singular[t]]}}.ts.tpl
+$ mkdir templates/sample/some-dir
+$ touch templates/sample/some-dir/{{kebabCase[plural[t]]}}.ts.tpl
+```
+
+**`sscg` can embed string into the file name by evaluating codes in `{{}}`.**
+
+Then, let's edit your `tpl` files.
+
+templates/sample/{{kebabCase[singular[t]]}}.ts.tpl:
+
+```ts
+export type {{pascalCase(singular(t))}} = {};
+```
+
+templates/sample/some-dir/{{kebabCase[plural[t]]}}.ts.tpl:
+
+```ts
+import type { {{pascalCase(singular(t))}} } from "../{{kebabCase(singular(t))}}";
+
+export type {{pascalCase(plural(t))}} = Array<{{pascalCase(singular(t))}}>;
+```
+
+**`sscg` can embed string into `tpl` files by evaluating codes in `{{}}`.**
+
+### 3. Generate codes from the your template
+
+You can generate code with the following command.
+
+```
+$ sscg sample -r sample_record -o outputs
+Generate from 'templates/sample'...
+
+outputs/sample-record.ts
+outputs/some-dir/sample-records.ts
+
+Done in 11ms
+```
+
+**The `-r` option is `replacement`, and it becomes `t` in `{{}}`.**
+
+outputs/sample-record.ts:
+
+```ts
+export type SampleRecord = {};
+```
+
+outputs/some-dir/sample-records.ts:
+
+```ts
+import type { SampleRecord } from "../sample-record";
+
+export type SampleRecords = Array<SampleRecord>;
+```
+
+## Supported variables
+
+|              | kind     | desc                                    |
+|--------------|----------|-----------------------------------------|
+| t            | value    | A string from the `-r` option           |
+| plural       | function | Pluralize a string                      |
+| singular     | function | Singularize a string                    |
+| camelCase    | function | Transform a string like `sampleRecord`  |
+| constantCase | function | Transform a string like `SAMPLE_RECORD` |
+| kebabCase    | function | Transform a string like `sample-record` |
+| pascalCase   | function | Transform a string like `SampleRecord`  |
+| snakeCase    | function | Transform a string like `sample_record` |
+
+## Usage
+
+```
+The super simple code generator
+
+Usage:
+  $ sscg <name> [options]
+
+Options:
+  -r, --replacement <text>    The string that replaces tokens in templates
+  -o, --out <dir>             The output directory where generated codes are located
+  -d, --dir <dir>             The directory where templates are located (default: ./templates)
+
+Example:
+  $ sscg model -r user -o ./models
+```
---
+++ src/index.mjs
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+import { mkdir, readFile, readdir, writeFile } from "node:fs/promises";
+import { dirname, extname, join, relative } from "node:path";
+import chalk from "chalk";
+import {
+  camelCase,
+  constantCase,
+  kebabCase,
+  pascalCase,
+  snakeCase,
+} from "change-case";
+import meow from "meow";
+import pluralize from "pluralize";
+const { plural, singular } = pluralize;
+
+const HELPERS = {
+  plural,
+  singular,
+  camelCase,
+  constantCase,
+  kebabCase,
+  pascalCase,
+  snakeCase,
+};
+
+function compile(opts, str) {
+  const ctx = { t: opts.replacement, ...HELPERS };
+  return str.replace(/\{\{(.+?)\}\}/g, (_, expression) => {
+    const exp = expression.trim();
+    try {
+      const func = new Function(...Object.keys(ctx), `return ${exp};`);
+      return func(...Object.values(ctx));
+    } catch (error) {
+      console.error(chalk.red(`Evaluation was failed:\n  ${exp}`));
+      process.exit(1);
+    }
+  });
+}
+
+function compilePath(opts, tplDir, tplPath) {
+  const preprocessed = join(opts.out, relative(tplDir, tplPath))
+    .replace(/\.tpl$/, "")
+    .replace(/\[/g, "(")
+    .replace(/\]/g, ")");
+  return compile(opts, preprocessed);
+}
+
+async function generate(opts, tplDir, dirent) {
+  const { name, parentPath } = dirent;
+  const tplPath = join(parentPath, name);
+  const tplBody = await readFile(tplPath, { encoding: "utf8" });
+  const content = compile(opts, tplBody);
+  const outputPath = compilePath(opts, tplDir, tplPath);
+  await mkdir(dirname(outputPath), { recursive: true });
+  await writeFile(outputPath, content, { encoding: "utf8" });
+  console.log(chalk.green(outputPath));
+}
+
+async function sscg(opts, tplName) {
+  const startedAt = new Date();
+  const tplDir = join(opts.dir, tplName);
+  console.log(`Generate from '${chalk.blue(tplDir)}'...\n`);
+  try {
+    const dirents = await readdir(tplDir, {
+      recursive: true,
+      withFileTypes: true,
+    });
+    const targets = dirents.filter(
+      (d) => d.isFile() && extname(d.name) === ".tpl",
+    );
+    await Promise.all(targets.map((t) => generate(opts, tplDir, t)));
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error(chalk.red(`'${tplDir}' was not found`));
+      process.exit(1);
+    }
+    throw err;
+  }
+  const finishedAt = new Date();
+  console.log(`\nDone in ${finishedAt - startedAt}ms`);
+}
+
+const { input, flags } = meow(
+  `
+    Usage:
+      $ sscg <name> [options]
+
+    Options:
+      -r, --replacement <text>    The string that replaces tokens in templates
+      -o, --out <dir>             The output directory where generated codes are located
+      -d, --dir <dir>             The directory where templates are located (default: ./templates)
+
+    Example:
+      $ sscg model -r user -o ./models
+  `,
+  {
+    importMeta: import.meta,
+    flags: {
+      replacement: {
+        type: "string",
+        shortFlag: "r",
+        isRequired: true,
+      },
+      out: {
+        type: "string",
+        shortFlag: "o",
+        isRequired: true,
+      },
+      dir: {
+        type: "string",
+        shortFlag: "d",
+        default: "./templates",
+        isRequired: true,
+      },
+    },
+  },
+);
+
+if (input.length <= 0) {
+  console.error(chalk.red("Template name is required"));
+  process.exit(1);
+}
+
+sscg(flags, input[0]);
